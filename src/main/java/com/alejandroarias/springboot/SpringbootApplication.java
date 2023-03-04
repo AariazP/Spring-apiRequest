ae816230ccb0c2e923c0b6ac833db3be49c0772f
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2023-03-04 17:13:34 -0500

    Primeros pasos

---
+++ src/main/java/com/alejandroarias/springboot/SpringbootApplication.java
@@ -0,0 +1,40 @@
+package com.alejandroarias.springboot;
+
+import com.alejandroarias.springboot.entities.Book;
+import com.alejandroarias.springboot.repositories.BookRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(SpringbootApplication.class, args);
+        BookRepository bookRepository = context.getBean(BookRepository.class);
+
+        System.out.println(" -----------------------Inicio -------------------");
+        System.out.println("El total de libros: "+bookRepository.findAll().size());
+
+        Book book1 = new Book(null, "El principito", "Antoine de Saint-Exupéry", 100, 10.0, LocalDate.of(12, 2, 4), true);
+        Book book2 = new Book(null, "Los juegos del hambre", "Suzanne Collins", 200, 20.0, LocalDate.of(12, 2, 4), true);
+
+        System.out.println(" -----------------------Insertar -------------------");
+
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+
+        System.out.println("El total de libros: "+bookRepository.findAll().size());
+
+
+        System.out.println(" -----------------------Eliminar -------------------");
+        System.out.println(" Elimino un libro con el id "+book1.getId() );
+        bookRepository.deleteById(book1.getId());
+
+        System.out.println("El total de libros: "+bookRepository.findAll().size());
+    }
+
+}
---
+++ src/main/java/com/alejandroarias/springboot/entities/Book.java
@@ -0,0 +1,41 @@
+package com.alejandroarias.springboot.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Integer pages;
+    private Double price;
+    private LocalDate releaseDate;
+    private boolean online;
+
+
+    public Book(Long id, String title, String author, Integer pages, Double price, LocalDate releaseDate, boolean online) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.price = price;
+        this.releaseDate = releaseDate;
+        this.online = online;
+    }
+
+    public Book() {
+    }
+
+
+}
